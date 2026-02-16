f5a66bcfe7fceee784e466bfe34497dcd5e594b9
Mac Wang <wang@Macs-MacBook-Air.local>
2026-02-16 15:33:05 -0500

    ephemeral

--- airbnb/models/src/src_reviews.sql
+++ airbnb/models/src/src_reviews.sql
@@ -5,6 +5,7 @@ SELECT
     listing_id,
     date AS review_date,
     reviewer_name,
-    comments AS review_texst,
+    comments AS review_text,
     sentiment AS review_sentiment
 FROM raw_reviews
+
